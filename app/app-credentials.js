82924333def9e7aae3d06b55b893d3ce47eb5f62
Agustín <xenrak@gmail.com>
2016-12-06 00:28:57 -0300

    API requested

---
+++ app/app-credentials.js
@@ -0,0 +1,6 @@
+app.constant("env",{
+  "APIKEY"          : "XXXXXXXXXXXXXXXXXXXXXXX",
+  "APIDIR"          : "https://osu.ppy.sh/api/",
+  "BMIMGURL"        : "http://b.ppy.sh/thumb/",
+  "USERIMGURL"      : "https://a.ppy.sh/"
+});
